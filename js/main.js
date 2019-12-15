3d636939c3b94b6c0bd979c0dff64a1f698169c2
Filip Troníček <filip.tronicek@seznam.cz>
2019-12-15 10:17:37 +0100

    Ability to change date and name

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let currentState = "all";
-
+/*
 const http = new XMLHttpRequest();
 
 http.open("GET", "https://time-server.now.sh/api/");
@@ -7,7 +7,7 @@ http.send();
 http.onload = () => {
     console.log("Server time: " + (http.responseText) );
 };
-
+*/
 function writeOnOut(text) {
   document.getElementById("time_to").innerHTML = text;
 }
@@ -24,55 +24,67 @@ document.getElementById("time_to").onclick = function() {
   } else if (currentState == "seconds") {
     currentState = "all";
   }
-  console.log("Changed to: " + currentState);
+  console.log("Layout: " + currentState);
 };
 
-var countDownDate = new Date("Dec 24, 2019 19:00").getTime();
+var urlParams = new URLSearchParams(window.location.search);
 
-// Update the count down every 1 second
-var x = setInterval(function() {
-  // Get todays date and time
-  var now = new Date().getTime();
+console.log(`Countdown date: ${urlParams.get("d")}`);
+console.log(`Countdown name: ${urlParams.get("n")}`);
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+if (urlParams.has("d") && urlParams.has("n")) {
+  const countDownDate = new Date(urlParams.get("d")).getTime();
+  const countDownName = urlParams.get("n");
+  document.getElementById("event_name").innerHTML = countDownName;
+  // Update the count down every 100 miliseconds
+  var x = setInterval(function() {
+    // Get todays date and time
+    var now = new Date().getTime();
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
 
-  var totalHours = days * 24 + hours;
-  var totalMinutes = totalHours * 60 + minutes;
-  var totalSeconds = totalMinutes * 60 + seconds;
-
-  if (currentState == "all") {
-    writeOnOut(
-      days +
-        " days " +
-        hours +
-        " hours " +
-        minutes +
-        " minutes " +
-        seconds +
-        " seconds "
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
     );
-  } else if (currentState == "days") {
-    writeOnOut(days.toLocaleString() + " days");
-  } else if (currentState == "hours") {
-    writeOnOut(totalHours.toLocaleString() + " hours");
-  } else if (currentState == "minutes") {
-    writeOnOut(totalMinutes.toLocaleString() + " minutes");
-  } else if (currentState == "seconds") {
-    writeOnOut(totalSeconds.toLocaleString() + " seconds");
-  }
-  // Output the result in an element with id="demo"
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // If the count down is over, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("time_to").innerHTML =
-      "This countdown has expired";
-  }
-}, 100);
+    var totalHours = days * 24 + hours;
+    var totalMinutes = totalHours * 60 + minutes;
+    var totalSeconds = totalMinutes * 60 + seconds;
+
+    if (currentState == "all") {
+      writeOnOut(
+        days +
+          " days " +
+          hours +
+          " hours " +
+          minutes +
+          " minutes " +
+          seconds +
+          " seconds "
+      );
+    } else if (currentState == "days") {
+      writeOnOut(days.toLocaleString() + " days");
+    } else if (currentState == "hours") {
+      writeOnOut(totalHours.toLocaleString() + " hours");
+    } else if (currentState == "minutes") {
+      writeOnOut(totalMinutes.toLocaleString() + " minutes");
+    } else if (currentState == "seconds") {
+      writeOnOut(totalSeconds.toLocaleString() + " seconds");
+    }
+    // Output the result in an element with id="demo"
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("time_to").innerHTML =
+        "This countdown has expired";
+    }
+  }, 100);
+} else {
+    alert("This event hasn't been set");
+}

208a35d7baa4e68e133613bedba5938fea564c34
Filip Troníček <filip.tronicek@seznam.cz>
2020-01-07 19:27:33 +0100

    Update redirect.js

--- js/redirect.js
+++ js/redirect.js
@@ -13,12 +13,12 @@ if(location.href.split("/")[3] == "e") urlSearch = location.href.split("/")[4];
 
 
 if (urlSearch !== "") {
-  eventName = urlParams.get("e");
+  eventName = urlSearch;
   if (eventName == "christmas-eve") {
     location.href = `../?d=12/24/${year} 19:00&n=🎄 Christmas ${year} 🎄`;
   } else if (eventName == "christmas") {
     location.href = `../?d=12/25/${year} 8:00&n=🎄 Christmas ${year} 🎄`;
-  } else if (eventName == "new-years") {
+  } else if (eventName == "new-years" || eventName == "nwyr") {
     location.href = `../?d=1/1/${year + 1} 0:00&n= Year ${year + 1} `;
   }
 } else {

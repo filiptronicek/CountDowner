ad5deed436c7b1057e70e685a1a2998ff21fb8bf
Filip Troníček <filip.tronicek@seznam.cz>
2020-11-08 15:59:47 +0100

    Arrow functions

--- js/create.js
+++ js/create.js
@@ -14,17 +14,17 @@ if (month == 11) {
 
 const instance = M.Timepicker.getInstance(document.querySelectorAll(".timepicker"));
 
-document.addEventListener("DOMContentLoaded", function () {
-    options = {
-        format: "mm/dd/yyyy"
-    };
-});
-document.addEventListener("DOMContentLoaded", function () {
-    options = {
-        twelveHour: false
-    };
-    var elems = document.querySelectorAll(".timepicker");
-});
+document.addEventListener("DOMContentLoaded", () => {
+        options = {
+            format: "mm/dd/yyyy"
+        };
+    });
+document.addEventListener("DOMContentLoaded", () => {
+        options = {
+            twelveHour: false
+        };
+        var elems = document.querySelectorAll(".timepicker");
+    });
 
 const patt = /^(?:(?:(?:0?[13578]|1[02])(\/|-|\.)31)\1|(?:(?:0?[1,3-9]|1[0-2])(\/|-|\.)(?:29|30)\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:0?2(\/|-|\.)29\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:(?:0?[1-9])|(?:1[0-2]))(\/|-|\.)(?:0?[1-9]|1\d|2[0-8])\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/g
 

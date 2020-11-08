4a129bd378d4be52438e72a595afbf514dba2804
Filip Troníček <filip.tronicek@seznam.cz>
2020-11-08 14:57:26 +0000

    Constant vars everywhere!

--- js/create.js
+++ js/create.js
@@ -1,7 +1,8 @@
-var date = new Date();
-var month = date.getMonth();
+const date = new Date();
+const month = date.getMonth();
+
 if (month == 11) {
-    var sf = new Snowflakes({
+    const sf = new Snowflakes({
         count: 30,
         maxSize: 20,
         speed: 1.5,
@@ -24,17 +25,15 @@ document.addEventListener("DOMContentLoaded", function () {
     };
     var elems = document.querySelectorAll(".timepicker");
 });
-var patt = /^(?:(?:(?:0?[13578]|1[02])(\/|-|\.)31)\1|(?:(?:0?[1,3-9]|1[0-2])(\/|-|\.)(?:29|30)\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:0?2(\/|-|\.)29\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:(?:0?[1-9])|(?:1[0-2]))(\/|-|\.)(?:0?[1-9]|1\d|2[0-8])\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/g
+
+const patt = /^(?:(?:(?:0?[13578]|1[02])(\/|-|\.)31)\1|(?:(?:0?[1,3-9]|1[0-2])(\/|-|\.)(?:29|30)\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:0?2(\/|-|\.)29\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:(?:0?[1-9])|(?:1[0-2]))(\/|-|\.)(?:0?[1-9]|1\d|2[0-8])\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/g
 
 function outPutInputs() {
-    let cntdDate = document.getElementById("date").value;
-    let cntdTime = document.getElementById("time").value;
-    let cntdName = document.getElementById("name").value;
+    const cntdDate = document.getElementById("date").value;
+    const cntdTime = document.getElementById("time").value;
+    const cntdName = document.getElementById("name").value;
 
-    console.log(cntdDate);
-    console.log(cntdTime);
     if (cntdTime != "" && cntdDate != "" && cntdName != "" && patt.test(cntdDate)) {
-        console.log(patt.test(cntdDate));
         location.href = `/?d=${cntdDate + " " + cntdTime}&n=${cntdName}`;
     } else if (!patt.test(cntdDate)) {
         alert("Your date is in an incorrect format");

5b01d1dcdd67a341f3cca8a8ddf08445a962a5ba
Filip Troníček <filip.tronicek@seznam.cz>
2020-11-08 16:39:59 +0100

    Update vars and comment out validation

--- js/create.js
+++ js/create.js
@@ -17,14 +17,18 @@ const picker = datepicker(".datepicker", {});
 const patt = /^(?:(?:(?:0?[13578]|1[02])(\/|-|\.)31)\1|(?:(?:0?[1,3-9]|1[0-2])(\/|-|\.)(?:29|30)\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:0?2(\/|-|\.)29\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:(?:0?[1-9])|(?:1[0-2]))(\/|-|\.)(?:0?[1-9]|1\d|2[0-8])\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/g
 
 function outPutInputs() {
-    const cntdDate = new Date(document.getElementById("date").value).getTime();
-    const cntdTime = document.getElementById("time").value;
+    const cntdDate = new Date(document.getElementById("date").value);
+    const cntdTime = cntdDate.setHours(document.getElementById("time").value.split(":")[0] || 0, document.getElementById("time").value.split(":")[1] || 0);
     const cntdName = document.getElementById("name").value;
 
-    location.href = `/?d=${`${cntdDate}${cntdTime}`}&n=${cntdName}`;
+    const unixTo = cntdTime;
+
+    location.href = `/?d=${unixTo}&n=${cntdName}`;
+    /*
     if (cntdTime != "" && cntdDate != "" && cntdName != "" && patt.test(cntdDate)) {
         location.href = `/?d=${cntdDate + " " + cntdTime}&n=${cntdName}`;
     } else if (!patt.test(cntdDate)) {
         alert("Your date is in an incorrect format");
     }
+    */
 }

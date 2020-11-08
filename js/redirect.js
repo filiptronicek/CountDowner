8fb833ad0f2ddc663c82383c454d5ecf738ee196
Filip Troníček <filip.tronicek@seznam.cz>
2020-11-08 18:26:52 +0100

    ;

--- js/redirect.js
+++ js/redirect.js
@@ -14,16 +14,16 @@ if (eventName !== "") {
   let redir;
   switch(eventName) {
     case "christmas-eve":
-      location.href = `../?d=12/24/${year} 19:00&n=🎄 Christmas ${year} 🎄`
+      location.href = `../?d=12/24/${year} 19:00&n=🎄 Christmas ${year} 🎄`;
       break;
     case "christmas":
-      location.href = `../?d=12/25/${year} 8:00&n=🎄 Christmas ${year} 🎄`
+      location.href = `../?d=12/25/${year} 8:00&n=🎄 Christmas ${year} 🎄`;
       break;
     case "new-years":
-      location.href = `../?d=1/1/${year + 1} 0:00&n= Year ${year + 1} `
+      location.href = `../?d=1/1/${year + 1} 0:00&n= Year ${year + 1} `;
       break;
     case "nwyr":
-      location.href = `../?d=1/1/${year + 1} 0:00&n= Year ${year + 1} `
+      location.href = `../?d=1/1/${year + 1} 0:00&n= Year ${year + 1} `;
       break;
   }
 } else {

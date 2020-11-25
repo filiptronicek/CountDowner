2e9db96a133fa1383bf20bce81960ef4bf64d5e3
Filip Troníček <filip.tronicek@seznam.cz>
2020-11-25 10:25:02 +0100

    Create create.js as webpack file

--- public/js/create.js
+++ public/js/create.js
@@ -1,3 +1,13 @@
+/* Imports */
+
+/* CSS */
+import '/public/css/create.scss';
+import '/public/css/dark.scss';
+
+/* JS */
+import datepicker from 'js-datepicker';
+import 'magic-snowflakes';
+
 const date = new Date();
 const month = date.getMonth();
 
@@ -12,7 +22,7 @@ if (month === 11) {
     });
 }
 
-const picker = datepicker(".datepicker", {});
+datepicker(".datepicker", {});
 
 const patt = /^(?:(?:(?:0?[13578]|1[02])(\/|-|\.)31)\1|(?:(?:0?[1,3-9]|1[0-2])(\/|-|\.)(?:29|30)\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:0?2(\/|-|\.)29\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:(?:0?[1-9])|(?:1[0-2]))(\/|-|\.)(?:0?[1-9]|1\d|2[0-8])\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/g
 
@@ -34,3 +44,5 @@ function outPutInputs() {
     }
     */
 }
+
+document.getElementById("create-countdown").addEventListener("click", outPutInputs); 

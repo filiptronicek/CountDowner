2e150abcd146f31e7890d192553de8059f9e0996
Filip Troníček <filip.tronicek@seznam.cz>
2020-11-27 00:05:26 +0100

    Install types and add shorten API

---
+++ api/shorten.ts
@@ -0,0 +1,9 @@
+import { NowRequest, NowResponse } from '@vercel/node'
+import fetch from 'node-fetch';
+
+export default (request: NowRequest, response: NowResponse) => {
+  const { url = '/e/nwyr' } = request.query
+  const fullURL : string = `https://countdowner.now.sh/${url}`
+  
+  fetch(`https://link.mannoviny.cz/includes/api?url=${fullURL}`, {}).then(r => r.json()).then(rsp => response.status(200).send({result: rsp.result}))
+}

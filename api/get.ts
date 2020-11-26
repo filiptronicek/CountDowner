7ddc819d066a3fd63a0bf0fe29c153f86141947b
Filip Troníček <filip.tronicek@seznam.cz>
2020-11-27 00:28:15 +0100

    Prepare un-shortening and vercel config for all this

---
+++ api/get.ts
@@ -0,0 +1,7 @@
+import { NowRequest, NowResponse } from '@vercel/node'
+import fetch from 'node-fetch';
+
+export default (request: NowRequest, response: NowResponse) => {
+  const { code = '' } = request.query  
+  fetch(`https://link.mannoviny.cz/includes/get-api?code=${code}`, {}).then(r => r.json()).then(rsp => response.status(200).send({result: rsp.result}))
+}
--- public/js/redirect.js
+++ public/js/redirect.js
@@ -7,7 +7,9 @@ const year = date.getFullYear();
 let eventName = "";
 
 if(urlParams.has("e")) eventName = urlParams.get("e"); 
-if(location.href.split("/")[3] == "e") eventName = location.href.split("/")[4];
+if(location.href.split("/")[3] === "e") eventName = location.href.split("/")[4];
+
+console.log(urlParams.get("c"));
 
 
 if (eventName !== "") {
@@ -25,6 +27,8 @@ if (eventName !== "") {
       location.href = `../?d=${nwyrdate}&n= Year ${year + 1} `;
       break;
   }
+} else if (urlParams.has("c")) {
+  fetch(`/api/get?code=${urlParams.get("c")}`, {}).then(r => r.json).then(res => location.href = res.result);
 } else {
-  location.href = "../";
+  //location.href = "../";
 }

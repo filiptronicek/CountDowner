5ea96d72e09a98ff1f97a570b7240fea89c99bba
Filip Troníček <filip.tronicek@seznam.cz>
2020-11-26 11:40:41 +0100

    Stuff idk

--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,29 @@ module.exports = {
     entry: {
         index: '/public/js/main.js',
         redirect: '/public/js/redirect.js',
-        create: '/public/js/create.js',
+        create: '/public/js/create.js'
     },
-    output: {
-        //filename: 'main.js',
-        path: path.resolve(__dirname, 'public'),
+    output: { // filename: 'main.js',
+        path: path.resolve(__dirname, 'public')
     },
     module: {
         rules: [
-          {
-            test: /\.s[ac]ss$/i,
-            use: [
-              // Creates `style` nodes from JS strings
-              "style-loader",
-              // Translates CSS into CommonJS
-              "css-loader",
-              // Compiles Sass to CSS
-              "sass-loader",
-            ],
-          },
-        ],
-      },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ]
+            },
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        compress: true,
+        port: 9000
+    }
 };

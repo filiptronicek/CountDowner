018e81287d9a5117ed83e784ba5401f56b8f1752
Filip Troníček <filip.tronicek@seznam.cz>
2020-11-27 13:14:21 +0100

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -28,17 +28,6 @@ for(const tz of timezones) {
 
 const reducedOffsets = uniqby(minuteOffsets, 'name');
 
-/* Window width and height constants */
-/*
-const w = window,
-d = document,
-e = d.documentElement,
-g = d.getElementsByTagName('body')[0],
-
-
-width = w.innerWidth||e.clientWidth||g.clientWidth,
-height = w.innerHeight||e.clientHeight||g.clientHeight;
-*/
 
 /* DOM */
 const divInstall = document.getElementById("installContainer");

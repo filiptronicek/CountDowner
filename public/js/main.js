bbc96700e2fb16488f4e6ba0a92e218eac80d30e
Filip Troníček <filip.tronicek@seznam.cz>
2020-12-13 23:17:01 +0100

    Don't require d and n params, set defaults
    Don't redirect so much when user enters /, because LIghthouse and Google reeeeally dislike it. Also it is pretty un-nice on the client side

--- public/js/main.js
+++ public/js/main.js
@@ -117,69 +117,101 @@ document.getElementById("time_to").onclick = () => {
 
 const urlParams = new URLSearchParams(window.location.search);
 
-if (urlParams.has("d") && urlParams.has("n")) {
 
-    const toCountDownDate = parseInt(urlParams.get("d")) || urlParams.get("d");
-    const countDownDate = new Date(toCountDownDate).getTime();
-    const countDownName = urlParams.get("n");
+let desiredDFLTtime;
+let desiredDFLTname;
 
-    const confettiEf = urlParams.has("confetti") ? urlParams.get("confetti") === "true" ? true : false : true;
-  
-    document.title = `${countDownName} | CountDowner`;
+console.log(desiredDFLTtime, desiredDFLTname);
 
-    document.getElementById("event_name").innerText = countDownName;
+const year = new Date().getFullYear();
 
-    // Update the count down every 100 miliseconds
-    const x = setInterval(() => { 
-        // Get today's UNIX time
-        const now = new Date().getTime() - localStorage.getItem("offset");
+switch (month + 1) {
+    case 12:
+        if (date.getDate() > 24) {
+            desiredDFLTtime = new Date(`1/1/${year + 1} 0:00`).getTime();
+            desiredDFLTname = `New years 🎊`;
+        } else {
+            desiredDFLTtime = new Date(`12/24/${year} 19:00`).getTime();
+            desiredDFLTname = `🎁 Christmas Eve 🎄`;
+        }
+        break;
+    default: 
+        desiredDFLTtime = new Date(`1/1/${year + 1} 0:00`).getTime();
+        desiredDFLTname = `New years 🎊`;
+}
 
-        // Time calculations for days, hours, minutes and seconds
-        const months = dayjs(countDownDate).diff(now, "month");
-        //const years = dayjs(countDownDate).diff(now, "year");
+const toCountDownDate = parseInt(urlParams.get("d")) || urlParams.get("d") || desiredDFLTtime;
+const countDownDate = new Date(toCountDownDate).getTime();
+const countDownName = urlParams.get("n") || desiredDFLTname;
 
-        const daywithoutMdiff = dayjs(countDownDate).subtract(months, 'month');
+const confettiEf = urlParams.has("confetti") ? urlParams.get("confetti") === "true" ? true : false : true;
 
-        // Find the difference between now and the countdown date
-        const distance = countDownDate - now;
-        if (distance < 86400 * 1000 && output.innerHTML === "") {
-            //output.innerHTML += `Your countdown has been hit in:`;
-            for (const offset of reducedOffsets) {
-                const offsetedDate = dayjs().add(offset.o, 'minute');
-                const desiredDate = dayjs(countDownDate).add(dayjs(countDownDate).utcOffset(), 'minute');
+document.title = `${countDownName} | CountDowner`;
 
-                if (offsetedDate.format("MM/DD") === desiredDate.format("MM/DD") && offsetedDate.format("hh:mm:ss") === desiredDate.format("hh:mm:ss")) {
-                    timezoneText.innerHTML = "You just hit your countdown in these timezones:";
-                    output.style.display = "block";
-                    output.innerHTML += `<li>${offset.name}</li> <br>`;
-                    setTimeout(() => { output.innerHTML = ""; output.style.display = "none"; timezoneText.innerHTML = ""; }, 15000);
-                }
+document.getElementById("event_name").innerText = countDownName;
+
+// Update the count down every 100 miliseconds
+const x = setInterval(() => { 
+    // Get today's UNIX time
+    const now = new Date().getTime() - localStorage.getItem("offset");
+
+    // Time calculations for days, hours, minutes and seconds
+    const months = dayjs(countDownDate).diff(now, "month");
+    //const years = dayjs(countDownDate).diff(now, "year");
+
+    const daywithoutMdiff = dayjs(countDownDate).subtract(months, 'month');
+
+    // Find the difference between now and the countdown date
+    const distance = countDownDate - now;
+    if (distance < 86400 * 1000 && output.innerHTML === "") {
+        //output.innerHTML += `Your countdown has been hit in:`;
+        for (const offset of reducedOffsets) {
+            const offsetedDate = dayjs().add(offset.o, 'minute');
+            const desiredDate = dayjs(countDownDate).add(dayjs(countDownDate).utcOffset(), 'minute');
+
+            if (offsetedDate.format("MM/DD") === desiredDate.format("MM/DD") && offsetedDate.format("hh:mm:ss") === desiredDate.format("hh:mm:ss")) {
+                timezoneText.innerHTML = "You just hit your countdown in these timezones:";
+                output.style.display = "block";
+                output.innerHTML += `<li>${offset.name}</li> <br>`;
+                setTimeout(() => { output.innerHTML = ""; output.style.display = "none"; timezoneText.innerHTML = ""; }, 15000);
             }
         }
+    }
 
-        const correctedToWithoutMonthDistance = Math.floor((daywithoutMdiff - now) / (1000 * 60 * 60 * 24));
+    const correctedToWithoutMonthDistance = Math.floor((daywithoutMdiff - now) / (1000 * 60 * 60 * 24));
 
-        const totaldays = Math.floor(distance / (1000 * 60 * 60 * 24));
-        const days =  correctedToWithoutMonthDistance % 7;
-        const hours = Math.floor(((daywithoutMdiff - now) % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor(((daywithoutMdiff - now) % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor(((daywithoutMdiff - now) % (1000 * 60)) / 1000);
+    const totaldays = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const days =  correctedToWithoutMonthDistance % 7;
+    const hours = Math.floor(((daywithoutMdiff - now) % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor(((daywithoutMdiff - now) % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor(((daywithoutMdiff - now) % (1000 * 60)) / 1000);
 
-        const weeks = (correctedToWithoutMonthDistance - (correctedToWithoutMonthDistance % 7)) / 7;
+    const weeks = (correctedToWithoutMonthDistance - (correctedToWithoutMonthDistance % 7)) / 7;
 
-        const totalHours = totaldays * 24 + hours;
-        const totalMinutes = totalHours * 60 + minutes;
-        const displayTotalSeconds = totaldays * 24 * 60 * 60 + hours * 60 * 60 + minutes * 60 + seconds;
-        const displayTotalMinutes = totaldays * 24 * 60 + hours * 60 + minutes;
-        
-        if (currentState == "all") {
-            if (totaldays > 7) {
-                writeOnOut(`
-                ${
-                    months > 0 ? format("month", months) : ""
-                } ${ 
-                    weeks > 0 ? format("week", weeks) : ""
-                } ${
+    const totalHours = totaldays * 24 + hours;
+    const totalMinutes = totalHours * 60 + minutes;
+    const displayTotalSeconds = totaldays * 24 * 60 * 60 + hours * 60 * 60 + minutes * 60 + seconds;
+    const displayTotalMinutes = totaldays * 24 * 60 + hours * 60 + minutes;
+    
+    if (currentState == "all") {
+        if (totaldays > 7) {
+            writeOnOut(`
+            ${
+                months > 0 ? format("month", months) : ""
+            } ${ 
+                weeks > 0 ? format("week", weeks) : ""
+            } ${
+                format("day", days)
+            } ${
+                format("hour", hours)
+            } ${
+                format("minute", minutes)
+            } ${
+                format("second", seconds)
+            }`);
+        } else {
+            if (days > 0) {
+                writeOnOut(`${
                     format("day", days)
                 } ${
                     format("hour", hours)
@@ -189,10 +221,8 @@ if (urlParams.has("d") && urlParams.has("n")) {
                     format("second", seconds)
                 }`);
             } else {
-                if (days > 0) {
+                if (hours > 0) {
                     writeOnOut(`${
-                        format("day", days)
-                    } ${
                         format("hour", hours)
                     } ${
                         format("minute", minutes)
@@ -200,114 +230,91 @@ if (urlParams.has("d") && urlParams.has("n")) {
                         format("second", seconds)
                     }`);
                 } else {
-                    if (hours > 0) {
+                    writeOnOut(`${
+                        format("minute", minutes)
+                    } ${
+                        format("second", seconds)
+                    }`);
+                    if (minutes > 0) {
                         writeOnOut(`${
-                            format("hour", hours)
-                        } ${
                             format("minute", minutes)
                         } ${
                             format("second", seconds)
                         }`);
+                    } else if (minutes < 1 && seconds < 20) {
+                        writeOnOut(seconds + "," + Math.floor((distance % 1000) / 100) + " seconds ");
                     } else {
-                        writeOnOut(`${
-                            format("minute", minutes)
-                        } ${
-                            format("second", seconds)
-                        }`);
-                        if (minutes > 0) {
-                            writeOnOut(`${
-                                format("minute", minutes)
-                            } ${
-                                format("second", seconds)
-                            }`);
-                        } else if (minutes < 1 && seconds < 20) {
-                            writeOnOut(seconds + "," + Math.floor((distance % 1000) / 100) + " seconds ");
-                        } else {
-                            writeOnOut(seconds + " seconds ");
-                        }
+                        writeOnOut(seconds + " seconds ");
                     }
                 }
             }
-        } else if (currentState == "days") {
-            if (totaldays < 1) 
-                currentState = "hours";
-             else 
-                writeOnOut(`${
-                    format("day", totaldays, true)
-                }`);
-            
-        } else if (currentState == "hours") {
-            if (totalHours < 1) 
-                currentState = "minutes";
-             else 
-                writeOnOut(`${
-                    format("hour", totalHours, true)
-                }`);
-            
-        } else if (currentState == "minutes") {
-            if (totalMinutes < 1) 
-                currentState = "seconds";
-             else 
-                writeOnOut(`${
-                    format("minute", displayTotalMinutes, true)
-                }`);
-            
-        } else if (currentState == "seconds") {
+        }
+    } else if (currentState == "days") {
+        if (totaldays < 1) 
+            currentState = "hours";
+            else 
             writeOnOut(`${
-                format("second", displayTotalSeconds, true)
+                format("day", totaldays, true)
             }`);
-        }
-        // Output the result in an element with id="demo"
-        // If the count down is over, write some text, render some confetti
-        if (distance < 0) {
-            clearInterval(x);
-            writeOnOut("This countdown is over");
-            if (Math.abs(distance) < 60 * 1000 && confettiEf) {
-            setTimeout(() => {
-                const endConfetti = Date.now() + (5 * 1000);
-
-                // go Buckeyes!
-                const colors = ['#ff0000', '#FCCA4D'];
         
-                (function frame() {
-                confetti({
-                    particleCount: 2,
-                    angle: 60,
-                    spread: 55,
-                    origin: { x: 0 },
-                    colors: colors
-                });
-                confetti({
-                    particleCount: 2,
-                    angle: 120,
-                    spread: 55,
-                    origin: { x: 1 },
-                    colors: colors
-                });
+    } else if (currentState == "hours") {
+        if (totalHours < 1) 
+            currentState = "minutes";
+            else 
+            writeOnOut(`${
+                format("hour", totalHours, true)
+            }`);
         
-                if (Date.now() < endConfetti) {
-                    requestAnimationFrame(frame);
-                }
-                }());
-            }, 500);
-        }
-        }
-    }, 100);
-} else {
-    let redirString;
-    switch (month + 1) {
-        case 12:
-            if (date.getDate() > 24) 
-                redirString = "nwyr";
-             else 
-                redirString = "christmas-eve";
-            
-            break;
-        default: redirString = "nwyr";
+    } else if (currentState == "minutes") {
+        if (totalMinutes < 1) 
+            currentState = "seconds";
+            else 
+            writeOnOut(`${
+                format("minute", displayTotalMinutes, true)
+            }`);
+        
+    } else if (currentState == "seconds") {
+        writeOnOut(`${
+            format("second", displayTotalSeconds, true)
+        }`);
     }
-    const encoded = encodeURI(`/e/${redirString}`);
-    location.href = encoded;
-}
+    // Output the result in an element with id="demo"
+    // If the count down is over, write some text, render some confetti
+    if (distance < 0) {
+        clearInterval(x);
+        writeOnOut("This countdown is over");
+        if (Math.abs(distance) < 60 * 1000 && confettiEf) {
+        setTimeout(() => {
+            const endConfetti = Date.now() + (5 * 1000);
+
+            // go Buckeyes!
+            const colors = ['#ff0000', '#FCCA4D'];
+    
+            (function frame() {
+            confetti({
+                particleCount: 2,
+                angle: 60,
+                spread: 55,
+                origin: { x: 0 },
+                colors: colors
+            });
+            confetti({
+                particleCount: 2,
+                angle: 120,
+                spread: 55,
+                origin: { x: 1 },
+                colors: colors
+            });
+    
+            if (Date.now() < endConfetti) {
+                requestAnimationFrame(frame);
+            }
+            }());
+        }, 500);
+    }
+    }
+}, 100);
+
 
 function copyLink() {
 

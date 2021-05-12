7178a7ef0b9e87289ef856b09aa0837665690f4e
Filip Troníček <filip.tronicek@seznam.cz>
2021-05-12 09:50:24 +0200

    ;

--- api/shorten.ts
+++ api/shorten.ts
@@ -2,8 +2,8 @@ import { NowRequest, NowResponse } from '@vercel/node'
 import fetch from 'node-fetch';
 
 export default (request: NowRequest, response: NowResponse) => {
-  const { url = '/e/nwyr' } = request.query
-  const fullURL : string = `https://cntd.now.sh/${url}`
+  const { url = '/e/nwyr' } = request.query;
+  const fullURL : string = `https://cntd.now.sh/${url}`;
   
-  fetch(`https://interclip.app/includes/api?url=${fullURL}`, {}).then(r => r.json()).then(rsp => response.status(200).send({result: rsp.result}))
+  fetch(`https://interclip.app/includes/api?url=${fullURL}`, {}).then(r => r.json()).then(rsp => response.status(200).send({result: rsp.result}));
 }

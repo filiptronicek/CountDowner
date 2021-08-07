bccc09e2b2b2f2336b4cba727728c16cda3b2f39
Filip Troníček <filip.tronicek@seznam.cz>
2021-08-07 23:13:08 +0000

    Comments

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,14 +18,16 @@ const reduceOverview = (
   dateFrom: dayjs.Dayjs,
   dateTo: dayjs.Dayjs,
   diffs: Array<number>
-) => {
+): Array<number> => {
   const newDiffs: Array<number> = [];
 
   let index = 0;
   for (const diff of diffs) {
     if (index === 0) {
+      // For years, there is no "overflowing", so just keep the value
       newDiffs.push(diff);
     } else {
+      // For other units, use dayjs.subtract() to prevent overflowing values (13 months, 75 minutes, ..,)
       const reducedDate = dateTo.subtract(
         diffs[index === 0 ? index : index - 1],
         parameters[index === 0 ? index : index - 1]
@@ -48,6 +50,7 @@ const formatDiffs = (diffs: Array<number>): string => {
   for (const unit of diffs) {
     wentThrough.push(unit);
     if ((unit !== 0 && wentThrough !== []) || sum(wentThrough) !== 0) {
+      // If all previous and the current value are 0, don't add to the string, otherwise, add the formatted string
       outputValues.push(`${unit} ${parameters[index]}${unit === 1 ? "" : "s"}`);
     }
     index++;

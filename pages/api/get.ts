373d6c43a7fc40889bf51f584f02bef528e3bf8d
Filip Troníček <filip.tronicek@seznam.cz>
2021-08-08 16:57:33 +0200

    req, res

--- pages/api/get.ts
+++ pages/api/get.ts
@@ -2,14 +2,14 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import fetch from "node-fetch";
 import { decode } from "js-base64";
 
-const get = (request: VercelRequest, response: VercelResponse) => {
-  const { code = "" } = request.query;
+const get = (req: VercelRequest, res: VercelResponse) => {
+  const { code = "" } = req.query;
   fetch(`https://interclip.app/includes/get-api?code=${code}`, {})
     .then((r) => r.json())
     .then((rsp) => {
       const URLparts = rsp.result.split("/");
       const constructedURL = `https://${URLparts[2]}/${decode(URLparts.pop())}`;
-      response.status(200).send({ result: constructedURL });
+      res.status(200).send({ result: constructedURL });
     });
 };
 

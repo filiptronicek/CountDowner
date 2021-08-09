8a5d848a271d76606a58eabd002a5d24911d8062
Filip Troníček <filip.tronicek@seznam.cz>
2021-08-09 17:40:26 +0200

    Stuff on the create page

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,10 +1,12 @@
 import dayjs from "dayjs";
-import { useRouter } from "next/router";
-import React, { useState, useEffect } from "react";
-import _toast, { Toaster } from "react-hot-toast";
+import React, { useState } from "react";
+import _toast, { toast, Toaster } from "react-hot-toast";
+import DatePicker from "react-datepicker";
+import QRCode from "react-qr-code";
 
-import getFormattedDiffs from "../lib/dateManipulation";
-import EventName from "../components/EventName";
+import unidecode from "unidecode";
+import { createEvent } from "ics";
+
 import Head from "../components/Head";
 import Menu from "../components/Menu";
 
@@ -15,7 +17,58 @@ import "react-datepicker/dist/react-datepicker.css";
 import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
 
+const Button = ({ onClick, children }) => {
+  return (
+    <span
+      className="bg-[#262A2B] text-white p-5 rounded-xl mb-8 cursor-pointer"
+      onClick={onClick}
+    >
+      {children}
+    </span>
+  );
+};
+
 export default function Home() {
+  const [date, setDate] = useState<Date>(new Date());
+
+  const defaultEventName = "";
+  const [eventName, setName] = useState<string>(defaultEventName);
+  const [qrCode, setQrCode] = useState<boolean>(false);
+
+  const downloadIcal = () => {
+    createEvent(
+      {
+        title: eventName,
+        busyStatus: "FREE",
+        start: [
+          date.getFullYear(),
+          date.getMonth() + 1,
+          date.getDate(),
+          date.getHours(),
+          date.getMinutes(),
+        ],
+        duration: { minutes: 60 },
+      },
+      (error, value) => {
+        if (error) {
+          console.log(error);
+        }
+        // Create a blob of the value and save it as event.ics
+        const blob = new Blob([value], { type: "text/calendar" });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+
+        const fileName = `${unidecode(eventName)}.ics`
+          .toLowerCase()
+          .replaceAll(" ", "-");
+
+        a.download = fileName;
+        a.click();
+      }
+    );
+  };
+
   return (
     <div className="flex flex-col items-center justify-between h-screen">
       <Head />
@@ -24,7 +77,60 @@ export default function Home() {
       <main className="text-center">
         <Toaster />
         <h1 className="text-3xl">Create a new countdown</h1>
-        
+        <label htmlFor="name">Event name: </label>
+        <input
+          id="name"
+          type="text"
+          className="w-1/2 p-3 font-thin transition duration-200 focus:shadow-md focus:outline-none ring-offset-2 border border-gray-400 rounded-lg focus:ring-2 focus:ring-gray-500"
+          value={eventName}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <br />
+        <DatePicker
+          dateFormat="dd/MM/yyyy"
+          selected={date}
+          showTimeSelect
+          timeIntervals={15}
+          minDate={new Date()}
+          onChange={(val: Date) => {
+            setDate(val);
+          }}
+        />
+
+        {defaultEventName !== eventName ? (
+          <div id="output" className="flex flex-col items-center mt-4">
+            <h2 className="text-4xl">{eventName}</h2>
+            <h3 className="text-4xl mb-8">
+              {dayjs(date).format("dddd, D MMMM YYYY (HH:mm)")}
+            </h3>
+            <div id="actions" className="flex gap-5">
+              <Button
+                onClick={() => {
+                  setQrCode(!qrCode);
+                }}
+              >
+                {qrCode ? "Hide" : "Show"} QR code
+              </Button>
+              <Button
+                onClick={() => {
+                  toast.success("Event created!");
+                  downloadIcal();
+                }}
+              >
+                Download .ics
+              </Button>
+            </div>
+            {qrCode && (
+              <QRCode
+                value={`https://countdowner.now.sh/?date=${date.getTime()}&name=${eventName}`}
+              />
+            )}
+          </div>
+        ) : (
+          <span> Please fill in the name and date of your event </span>
+        )}
       </main>
 
       <footer>By @filiptronicek with 💖</footer>

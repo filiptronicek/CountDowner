72e8e2b3f1976209a77606cc711a861898c49758
Filip Troníček <filip.tronicek@seznam.cz>
2021-08-17 21:48:47 +0000

    More contributors!

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,12 +3,27 @@ import { useTranslation } from "react-i18next";
 
 const Footer = (): JSX.Element => {
   const { t, i18n } = useTranslation();
+  const contributors = ["filiptronicek", "krystofex"];
 
   return (
     <footer>
-      <Link href="https://github.com/filiptronicek">
-       {`${t("By")} @filiptronicek with 💖`}
-      </Link>
+      <span>
+        {`${t("By")}`}{" "}
+        {contributors.map((contributor) => {
+          const index = contributors.indexOf(contributor);
+          return (
+            <Link href={`https://github.com/${contributor}`}>
+              <span>
+                @
+                {`${contributor}${
+                  index !== contributors.length - 1 ? ", " : ""
+                }`}
+              </span>
+            </Link>
+          );
+        })}{" "}
+        with 💖
+      </span>
     </footer>
   );
 };

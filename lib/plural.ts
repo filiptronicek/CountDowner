2e59e078f629a1c9d84a9e05fec8f0213a150232
Filip Troníček <filip.tronicek@seznam.cz>
2021-09-23 11:50:13 +0200

    Translate plurals

--- lib/plural.ts
+++ lib/plural.ts
@@ -1,14 +1,17 @@
+import { useTranslation } from "react-i18next";
+const { t } = useTranslation();
+
 const plural = (word: string, count: number): string => {
   if (word === undefined) {
     return "";
   }
   if (count === 1) {
-    return word;
+    return t(word);
   }
   if (word.endsWith("s")) {
-    return word + "es";
+    return t(word + "es");
   }
-  return word + "s";
+  return t(word + "s");
 };
 
 export default plural;

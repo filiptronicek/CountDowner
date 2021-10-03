738058c778356933094586717aded3a45d903dc4
Filip Troníček <filip@gitpod.io>
2021-10-03 12:22:28 +0000

    deps: Upgrade to Tailwind CSS v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "media",
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'media',
   theme: {
     extend: {
       boxShadow: {
-        custom: "0px 3px 12px rgba(0, 0, 0, 0.48)",
+        custom: '0px 3px 12px rgba(0, 0, 0, 0.48)',
       },
     },
   },

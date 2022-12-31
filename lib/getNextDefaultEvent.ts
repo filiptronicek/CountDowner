fbb0c480fc49ba33b316f27cdd4fa714b963ed5e
Filip Troníček <filip@gitpod.io>
2022-12-31 11:09:27 +0000

    Am I dumb or just misunderstood

--- lib/getNextDefaultEvent.ts
+++ lib/getNextDefaultEvent.ts
@@ -10,7 +10,7 @@ const getNextDefaultEvent = (now: Date): [string, Date] => {
   const day = now.getDate();
 
   // If Christmas was this year already, return New Year's Day
-  if (month === 12 && day > 24) {
+  if (month === 11 && day > 24) {
     return ['New Years', new Date(year + 1, 0, 1)];
   } else {
     return ['Christmas', new Date(year, 11, 25)];
